4bc9866844d8ed5e34b5c272fca6bc675346d5fa
Lucas Martins <83207781+LucasSMartins@users.noreply.github.com>
2022-07-16 10:23:38 -0300

    foi organizado a estrutura de pastas

--- __test/pages/utils/index.js
+++ __test/pages/utils/index.js
@@ -1,4 +1,4 @@
-// getdropbox
+
 // const limpo = document.querySelector("#limpo");
 // const parceria = document.querySelector("#parceria");
 // const sujo = document.querySelector("sujo");
@@ -7,7 +7,14 @@
 
 
 function calcular(un, preco) {
-  return un * preco 
+  const valores = []
+
+  for (let item in preco) {
+
+    valores[item] = un * preco[item]
+    console.log(valores[item])
+  }
+
 }
 
 
@@ -37,33 +44,59 @@ function resultado(preco) {
   c.innerHTML += `C4 x${unC4}.....Total:${calcular(unC4, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
   c.innerHTML += `Molas x${unMolas}.....Total:${calcular(unMolas, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
   c.innerHTML += `Gatilhos x${unGatilhos}.....Total:${calcular(unGatilhos, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `PlacadeMetal x${unPlacadeMetal}.....Total:${calcular(unPlacadeMetal, preco).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}.<br/><br/>`
+  c.innerHTML += `PlacadeMetal x${unPlacadeMetal}.....Total:${calcular(unPlacadeMetal, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/><br/>`
 }
 
 function verificarTipo() {
-  const tipo = document.getElementById("tipo").value; 
+  const tipo = document.getElementById("tipo").value;
 
   if (tipo == "limpo") {
-    /*let preco = {
-      keycard:5000,
-      Lockpick:20000,
-      Capuz:20000,
-      Algemas:20000,
-      Colete:35000,
-      Placa:40000,
-      Pendrive:40000,
-      C4:50000,
+    let preco = {
+      keycard: 5000,
+      Lockpick: 20000,
+      Capuz: 20000,
+      Algemas: 20000,
+      Colete: 35000,
+      Placa: 40000,
+      Pendrive: 40000,
+      C4: 50000,
     }
-    resultado(preco)*/
-
+    resultado(preco)
   } else if (tipo == "parceria") {
-    let preco = 15000;
+    let preco = {
+      keycard: 5000,
+      Lockpick: 15000,
+      Capuz: 15000,
+      Algemas: 15000,
+      Colete: 25000,
+      Placa: 30000,
+      Pendrive: 30000,
+      C4: 40000,
+    }
     resultado(preco)
   } else if (tipo == "sujo") {
-    let preco = 35000;
+    let preco = {
+      keycard: 5000,
+      Lockpick: 35000,
+      Capuz: 35000,
+      Algemas: 35000,
+      Colete: 55000,
+      Placa: 60000,
+      Pendrive: 60000,
+      C4: 80000,
+    }
     resultado(preco)
   } else if (tipo == "sujoParceria") {
-    let preco = 25000;
+    let preco = {
+      keycard: 5000,
+      Lockpick: 25000,
+      Capuz: 25000,
+      Algemas: 25000,
+      Colete: 40000,
+      Placa: 50000,
+      Pendrive: 50000,
+      C4: 70000,
+    }
     resultado(preco)
   } else {
     alert("Deu tudo errado man");

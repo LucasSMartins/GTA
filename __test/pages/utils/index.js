ef0368fbdc948c55eb983431cf100aa38352ebdd
Lucas Martins <83207781+LucasSMartins@users.noreply.github.com>
2022-07-17 07:04:56 -0300

    16/07/2022

--- __test/pages/utils/index.js
+++ __test/pages/utils/index.js
@@ -1,58 +1,123 @@
+function calcular(unidade, preco) {
+  /*const total = {
+    totalKeycard: unidade.unKeycard * preco.Keycard,
+    totalLockpick: unidade.unLockpick * preco.Lockpick,
+    totalCapuz: unidade.unCapuz * preco.Capuz,
+    totalAlgemas: unidade.unAlgemas * preco.Algemas,
+    totalColete: unidade.unColete * preco.Colete,
+    totalPlaca: unidade.unPlaca * preco.Placa,
+    totalPendrive: unidade.unPendrive * preco.Pendrive,
+    totalC4: unidade.unC4 * preco.C4,
+    totalMolas: unidade.unMolas * preco.Molas,
+    totalGatilhos: unidade.unGatilhos * preco.Gatilhos,
+    totalPlacadeMetal: unidade.unPlacadeMetal * preco.PlacadeMetal,
+  };
 
-// const limpo = document.querySelector("#limpo");
-// const parceria = document.querySelector("#parceria");
-// const sujo = document.querySelector("sujo");
-// const sujoParceria = document.querySelector("#sujoParceria");
-// const tipo = document.document.querySelector("tipo");
+  return total;*/
+  const total = [
+    (totalKeycard = unidade.unKeycard * preco.Keycard),
+    (totalLockpick = unidade.unLockpick * preco.Lockpick),
+    (totalCapuz = unidade.unCapuz * preco.Capuz),
+    (totalAlgemas = unidade.unAlgemas * preco.Algemas),
+    (totalColete = unidade.unColete * preco.Colete),
+    (totalPlaca = unidade.unPlaca * preco.Placa),
+    (totalPendrive = unidade.unPendrive * preco.Pendrive),
+    (totalC4 = unidade.unC4 * preco.C4),
+    (totalMolas = unidade.unMolas * preco.Molas),
+    (totalGatilhos = unidade.unGatilhos * preco.Gatilhos),
+    (totalPlacadeMetal = unidade.unPlacadeMetal * preco.PlacadeMetal),
+  ];
 
-
-function calcular(un, preco) {
-  const valores = []
-
-  for (let item in preco) {
-
-    valores[item] = un * preco[item]
-    console.log(valores[item])
-  }
-
+  return total;
 }
 
+const resultado = (preco) => {
+  const c = document.querySelector("#c");
+  const d = document.querySelector("#d");
 
+  
+    const unidade = {
+    unKeycard: Number(document.querySelector("#Keycard").value),
+    unLockpick: Number(document.querySelector("#Lockpick").value),
+    unCapuz: Number(document.querySelector("#Capuz").value),
+    unAlgemas: Number(document.querySelector("#Algemas").value),
+    unColete: Number(document.querySelector("#Colete").value),
+    unPlaca: Number(document.querySelector("#Placa").value),
+    unPendrive: Number(document.querySelector("#Pendrive").value),
+    unC4: Number(document.querySelector("#C4").value),
+    unMolas: Number(document.querySelector("#Molas").value),
+    unGatilhos: Number(document.querySelector("#Gatilhos").value),
+    unPlacadeMetal: Number(document.querySelector("#PlacadeMetal").value),
+  };
+  /*
+  const item = {
+    Keycard: "Keycard",
+    Lockpick: "Lockpick",
+    Capuz: "Capuz",
+    Algemas: "Algemas",
+    Colete: "Colete",
+    Placa: "Placa",
+    Pendrive: "Pendrive",
+    C4: "C4",
+    Molas: "Molas",
+    Gatilhos: "Gatilhos",
+    PlacadeMetal: "Placa de Metal",
+  };
+  */
+  
+  const uni = [
+    unKeycard = Number(document.querySelector("#Keycard").value),
+    unLockpick = Number(document.querySelector("#Lockpick").value),
+    unCapuz = Number(document.querySelector("#Capuz").value),
+    unAlgemas = Number(document.querySelector("#Algemas").value),
+    unColete = Number(document.querySelector("#Colete").value),
+    unPlaca = Number(document.querySelector("#Placa").value),
+    unPendrive = Number(document.querySelector("#Pendrive").value),
+    unC4 = Number(document.querySelector("#C4").value),
+    unMolas = Number(document.querySelector("#Molas").value),
+    unGatilhos = Number(document.querySelector("#Gatilhos").value),
+    unPlacadeMetal = Number(document.querySelector("#PlacadeMetal").value),
+  ]
 
-function resultado(preco) {
-  const c = document.querySelector("#c")
-  let unKeycard = Number(document.querySelector("#Keycard").value);
-  let unLockpick = Number(document.querySelector("#Lockpick").value);
-  let unCapuz = Number(document.querySelector("#Capuz").value);
-  let unAlgemas = Number(document.querySelector("#Algemas").value);
-  let unColete = Number(document.querySelector("#Colete").value);
-  let unPlaca = Number(document.querySelector("#Placa").value);
-  let unPendrive = Number(document.querySelector("#Pendrive").value);
-  let unC4 = Number(document.querySelector("#C4").value);
-  let unMolas = Number(document.querySelector("#Molas").value);
-  let unGatilhos = Number(document.querySelector("#Gatilhos").value);
-  let unPlacadeMetal = Number(document.querySelector("#PlacadeMetal").value);
+  const item = [
+    "Keycard",
+    "Lockpick",
+    "Capuz",
+    "Algemas",
+    "Colete",
+    "Placa",
+    "Pendrive",
+    "C4",
+    "Molas",
+    "Gatilhos",
+    "Placa de Metal",
+  ];
 
+  const valor = calcular(unidade, preco);
+
+  /*c.innerHTML = "";
+  Object.keys(item).forEach((i) => {
+    c.innerHTML += `${item[i]}<br/>`;
+  });*/
   c.innerHTML = ""
-  c.innerHTML += `Keycard x${unKeycard}.....Total:${calcular(unKeycard, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Lockpick x${unLockpick}.....Total:${calcular(unLockpick, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Capuz x${unCapuz}.....Total:${calcular(unCapuz, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Algemas x${unAlgemas}.....Total:${calcular(unAlgemas, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Colete x${unColete}.....Total:${calcular(unColete, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Placa x${unPlaca}.....Total:${calcular(unPlaca, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Pendrive x${unPendrive}.....Total:${calcular(unPendrive, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `C4 x${unC4}.....Total:${calcular(unC4, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Molas x${unMolas}.....Total:${calcular(unMolas, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `Gatilhos x${unGatilhos}.....Total:${calcular(unGatilhos, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/>`
-  c.innerHTML += `PlacadeMetal x${unPlacadeMetal}.....Total:${calcular(unPlacadeMetal, preco).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.<br/><br/>`
-}
+  d.innerHTML = "";
+  Object.keys(item, valor).forEach((i) => {
+    if (valor[i] == 0) {
+      c.innerHTML += "";
+      d.innerHTML += "";
+    } else {
+      c.innerHTML += `${item[i]}   x${uni[i]} un<br/>`;
+      d.innerHTML += `${valor[i]}<br/>`;
+    }
+  });
+};
 
 function verificarTipo() {
   const tipo = document.getElementById("tipo").value;
 
   if (tipo == "limpo") {
-    let preco = {
-      keycard: 5000,
+    const preco = {
+      Keycard: 5000,
       Lockpick: 20000,
       Capuz: 20000,
       Algemas: 20000,
@@ -60,11 +125,14 @@ function verificarTipo() {
       Placa: 40000,
       Pendrive: 40000,
       C4: 50000,
-    }
-    resultado(preco)
+      Molas: 916500,
+      Gatilhos: 1098500,
+      PlacaDeMetal: 1020500,
+    };
+    resultado(preco);
   } else if (tipo == "parceria") {
-    let preco = {
-      keycard: 5000,
+    const preco = {
+      Keycard: 5000,
       Lockpick: 15000,
       Capuz: 15000,
       Algemas: 15000,
@@ -72,11 +140,14 @@ function verificarTipo() {
       Placa: 30000,
       Pendrive: 30000,
       C4: 40000,
-    }
-    resultado(preco)
+      Molas: 916500,
+      Gatilhos: 1098500,
+      PlacaDeMetal: 1020500,
+    };
+    resultado(preco);
   } else if (tipo == "sujo") {
-    let preco = {
-      keycard: 5000,
+    const preco = {
+      Keycard: 5000,
       Lockpick: 35000,
       Capuz: 35000,
       Algemas: 35000,
@@ -84,11 +155,14 @@ function verificarTipo() {
       Placa: 60000,
       Pendrive: 60000,
       C4: 80000,
-    }
-    resultado(preco)
+      Molas: 916500,
+      Gatilhos: 1098500,
+      PlacaDeMetal: 1020500,
+    };
+    resultado(preco);
   } else if (tipo == "sujoParceria") {
-    let preco = {
-      keycard: 5000,
+    const preco = {
+      Keycard: 5000,
       Lockpick: 25000,
       Capuz: 25000,
       Algemas: 25000,
@@ -96,8 +170,11 @@ function verificarTipo() {
       Placa: 50000,
       Pendrive: 50000,
       C4: 70000,
-    }
-    resultado(preco)
+      Molas: 916500,
+      Gatilhos: 1098500,
+      PlacaDeMetal: 1020500,
+    };
+    resultado(preco);
   } else {
     alert("Deu tudo errado man");
   }
